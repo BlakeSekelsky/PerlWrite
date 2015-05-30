3da5bc2758742b412bfb3e9d0e85747101f57493
Blake Sekelsky <sekelskyblake@gmail.com>
2015-05-30 13:41:14 -0400

    Create PerlWrite.java

---
+++ PerlWrite.java
@@ -0,0 +1,22 @@
+/*
+Computer Science Final Project
+
+PerlWrite.java
+Author: Blake Sekelsky
+
+*/
+
+import java.io.*;
+import java.awt.FlowLayout;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PerlWrite{
+   public static void main(String args[]) {
+       GUI g = new GUI();
+       g.createGUI();
+       g.sideGUI();
+    }
+}
