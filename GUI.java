ad0d5c964c126d33b02b9ef60e7b677c0d4b4b04
Blake Sekelsky <sekelskyblake@gmail.com>
2015-06-01 13:26:30 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -22,7 +22,6 @@ import java.nio.file.Paths;
 public class GUI
 {
    public JFrame frame = new JFrame("PerlWrite");
-   public JPanel panel = new JPanel();
    public JTextArea text = new JTextArea(); 
    public JScrollPane scroll = new JScrollPane(text);
    public String activeScript = "";
@@ -33,72 +32,46 @@ public class GUI
    public String file;
     
     void createGUI(){
-        //Setup panel
+        //System.setProperty("apple.laf.useScreenMenuBar", "true");
+        // Creates a menubar for a JFrame
+        JMenuBar menuBar = new JMenuBar();
+        JPanel panel = new JPanel();
+        
         panel.setLayout(new BorderLayout());
-        panel.add(scroll, BorderLayout.CENTER); 
-        
-        //Frame declarations
-        frame.add(panel);
-        frame.setResizable(false);
-        frame.setSize(640, 480);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-   }
-   
-   void sideGUI(){
-        //Create Window
-        JFrame frame2 = new JFrame("Actions");
-        
-        //Run Button
-        JButton runBtn = new JButton("Run Script");
-        runBtn.setHorizontalAlignment(SwingConstants.CENTER);
-        runBtn.setLocation(10, 80);
-        runBtn.setSize(105, 50);
-        
-        //Clear Console Button
-        JButton clrBtn = new JButton("Clear Console");
-        clrBtn.setHorizontalAlignment(SwingConstants.CENTER);
-        clrBtn.setLocation(125, 80);
-        clrBtn.setSize(105, 50);
-        
-        //New Script Button
-        JButton newBtn = new JButton("New Script");
-        newBtn.setHorizontalAlignment(SwingConstants.CENTER);
-        newBtn.setLocation(10, 20);
-        newBtn.setSize(220, 50);
-        
-        //Open Script Button
-        JButton opnBtn = new JButton("Open Script");
-        opnBtn.setHorizontalAlignment(SwingConstants.CENTER);
-        opnBtn.setLocation(10, 140);
-        opnBtn.setSize(105, 50);
-        
-        //Save Script Button
-        JButton svBtn = new JButton("Save Script");
-        svBtn.setHorizontalAlignment(SwingConstants.CENTER);
-        svBtn.setLocation(125, 140);
-        svBtn.setSize(105, 50);
-        
-        
-        
-        //Adding UI
-        frame2.add(clrBtn);
-        frame2.add(runBtn);
-        frame2.add(newBtn);
-        frame2.add(opnBtn);
-        frame2.add(svBtn);
+        panel.add(text);
+        frame.add(text);
+                 
+        // Define and add two drop down menu to the menubar
+        JMenu fileMenu = new JMenu("File");
+        JMenu Console = new JMenu("Console");
+        menuBar.add(fileMenu);
+        menuBar.add(Console);
+         
+        //Adding Menu Items
+        JMenuItem newAction = new JMenuItem("New");
+        JMenuItem saveAction = new JMenuItem("Save");
+        JMenuItem executeAction = new JMenuItem("Execute");
+        JMenuItem clearConsoleAction = new JMenuItem("Clear Console");
+        JMenuItem openAction = new JMenuItem("Open");
+        JMenuItem exitAction = new JMenuItem("Exit");
+         
+        ButtonGroup bg = new ButtonGroup();
+        fileMenu.add(newAction);
+        fileMenu.add(saveAction);
+        fileMenu.add(openAction);
+        fileMenu.addSeparator();
+        fileMenu.add(exitAction);
+        Console.add(executeAction);
+        Console.add(clearConsoleAction);
 
-        //Init Window
-        frame2.setLayout(null);
-        frame2.setResizable(false);
-        frame2.setSize(240, 240);        
-        frame2.setLocationRelativeTo(null);
-        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame2.setVisible(true);
+        //Listeners
+        clearConsoleAction.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                System.out.print("\f");
+            }
+        });
         
-        //Action Handlers
-        svBtn.addActionListener(new ActionListener(){
+        saveAction.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 try{
                     file = JOptionPane.showInputDialog(null, "Enter perl script name to save: ");
@@ -115,7 +88,7 @@ public class GUI
             }
         });
         
-        opnBtn.addActionListener(new ActionListener(){
+        openAction.addActionListener(new ActionListener(){
            public void actionPerformed(ActionEvent e){
                try{
                    file = JOptionPane.showInputDialog(null, "Enter perl script name to open: ");
@@ -138,25 +111,34 @@ public class GUI
             }
         });
         
-        newBtn.addActionListener(new ActionListener(){
+        newAction.addActionListener(new ActionListener(){
            public void actionPerformed(ActionEvent e){
                text.setText("");
             }
         });
         
-        clrBtn.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                System.out.print("\f");
-            }
-        });
-        
-        runBtn.addActionListener(new ActionListener(){
+        executeAction.addActionListener(new ActionListener(){
  
             public void actionPerformed(ActionEvent e){
                 executeScript(activeScript);
             }
         });
-    }
+        
+        exitAction.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+            }
+        });
+        
+        
+        //Frame declarations
+        frame.setJMenuBar(menuBar);
+        frame.setResizable(false);
+        frame.setSize(640, 480);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+   }
    
    void executeScript(String fileName){
         Process process;
