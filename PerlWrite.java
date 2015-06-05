a6dc4689b3bf443a80877b46324ca9eedffc4bb5
Blake Sekelsky <sekelskyblake@gmail.com>
2015-06-05 11:29:26 -0400

    Update PerlWrite.java

--- PerlWrite.java
+++ PerlWrite.java
@@ -1,20 +1,15 @@
 /*
-Follows the GNU Public License.
-
 Computer Science Final Project
 
 Author: Blake Sekelsky
 
+PerlWrite
+Program that allows you to run Perl(.pl) scripts. Basic text editor that allows perl scripting with saving,
+opening, as well as clearing console, and running the script.
+
 PerlWrite.java
 Nothing special, just initializes Program
 */
-
-import java.io.*;
-import java.awt.FlowLayout;
-import java.awt.*;
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class PerlWrite{
    public static void main(String args[]) {
